4f540047f1d42dc76c5b7d7bf1be00baba227d20
Zhang Yiming <zhangyiming@eoffice.com>
2022-08-29 21:36:53 +0800

    用户自定义文章主题和字数限制

--- bullshit_generator/index.js
+++ bullshit_generator/index.js
@@ -1,23 +1,106 @@
-import {readFileSync,existsSync,mkdirSync,writeFileSync} from 'fs';
-import {fileURLToPath} from 'url';
-import {dirname, resolve} from 'path';
-import moment from 'moment';
-
+import {options} from './lib/cmd.js';
+import {loadCorpus, saveCorpus} from './lib/corpus.js';
 import {generate} from './lib/generator.js';
 import {createRandomPicker} from './lib/random.js';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-function loadCorpus(src) {
-  const path = resolve(__dirname, src);
-  const data = readFileSync(path, {encoding: 'utf-8'});
-  return JSON.parse(data);
-}
-
 const corpus = loadCorpus('corpus/data.json');
+const title = options.title || createRandomPicker(corpus.title)();
+const article = generate(title, {corpus, ...options});
+const output = saveCorpus(title, article);
 
-const pickTitle = createRandomPicker(corpus.title);
-const title = pickTitle();
+console.log(`生成成功！文章保存于：${output}`);
+
+
+// import {readFileSync,existsSync,mkdirSync,writeFileSync} from 'fs';
+// import {fileURLToPath} from 'url';
+// import {dirname, resolve} from 'path';
+// import moment from 'moment';
+// import commandLineArgs from 'command-line-args';
+// import commandLineUsage from 'command-line-usage';
+
+// import {generate} from './lib/generator.js';
+// import {createRandomPicker} from './lib/random.js';
+
+// const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// function loadCorpus(src) {
+//   const path = resolve(__dirname, src);
+//   const data = readFileSync(path, {encoding: 'utf-8'});
+//   return JSON.parse(data);
+// }
+
+// function parseOptions(options = {}) {
+//     const argv = process.argv;
+//     for(let i = 2; i < argv.length; i++) {
+//       const cmd = argv[i - 1];
+//       const value = argv[i];
+//       if(cmd === '--title') {
+//         options.title = value;
+//       } else if(cmd === '--min') {
+//         options.min = Number(value);
+//       } else if(cmd === '--max') {
+//         options.max = Number(value);
+//       }
+//     }
+//     return options;
+//   }
+
+// 定义帮助的内容
+// const sections = [
+//     {
+//       header: '狗屁不通文章生成器',
+//       content: '生成随机的文章段落用于测试',
+//     },
+//     {
+//       header: 'Options',
+//       optionList: [
+//         {
+//           name: 'title',
+//           typeLabel: '{underline string}',
+//           description: '文章的主题。',
+//         },
+//         {
+//           name: 'min',
+//           typeLabel: '{underline number}',
+//           description: '文章最小字数。',
+//         },
+//         {
+//           name: 'max',
+//           typeLabel: '{underline number}',
+//           description: '文章最大字数。',
+//         },
+//       ],
+//     },
+//   ];
+//   const usage = commandLineUsage(sections); // 生成帮助文本
+
+//   const corpus = loadCorpus('corpus/data.json');
+//   // 配置我们的命令行参数
+//   const optionDefinitions = [
+//     {name: 'help'}, // help命令配置
+//     {name: 'title', type: String},
+//     {name: 'min', type: Number},
+//     {name: 'max', type: Number},
+//   ];
+//   const options = commandLineArgs(optionDefinitions); // 获取命令行的输入
+//   if('help' in options) { // 如果输入的是help，就打印帮助文本
+//     console.log(usage);
+//   } else {
+//     const title = options.title || createRandomPicker(corpus.title)();
+//     const article = generate(title, {corpus, ...options});
+//     const output = saveCorpus(title, article);
+//     console.log(`生成成功！文章保存于：${output}`);
+//   }
+
+//   const corpus = loadCorpus('corpus/data.json');
+//   const options = parseOptions();
+//   const title = options.title || createRandomPicker(corpus.title)();
+//   const article = generate(title, {corpus, ...options});
+//   const output = saveToFile(title, article);
+//   console.log(`生成成功！文章保存于：${output}`);
+
+// const pickTitle = createRandomPicker(corpus.title);
+// const title = pickTitle();
 
 // const pickFamous = createRandomPicker(corpus.famous);
 // const pickBosh = createRandomPicker(corpus.bosh);
@@ -25,27 +108,27 @@ const title = pickTitle();
 // pickFamous(); // 随机取出一条名人名言
 // pickBosh(); // 随机取出一条废话
 
-const article = generate(title, {corpus});
+// const article = generate(title, {corpus});
 // console.log(`${title}\n\n    ${article.join('\n    ')}`);
 
 // 封装一个保存文件的函数
-function saveCorpus(title, article) {
-    const outputDir = resolve(__dirname, 'output');
-    const time = moment().format('mmss');
-    const outputFile = resolve(outputDir, `${title}${time}.txt`);
+// function saveCorpus(title, article) {
+//     const outputDir = resolve(__dirname, 'output');
+//     const time = moment().format('mmss');
+//     const outputFile = resolve(outputDir, `${title}${time}.txt`);
   
-    // 检查outputDir是否存在，没有则创建一个
-    if(!existsSync(outputDir)) {
-      mkdirSync(outputDir);
-    }
+//     // 检查outputDir是否存在，没有则创建一个
+//     if(!existsSync(outputDir)) {
+//       mkdirSync(outputDir);
+//     }
   
-    const text = `${title}\n\n    ${article.join('\n    ')}`;
-    writeFileSync(outputFile, text); // 将text写入outputFile文件中
+//     const text = `${title}\n\n    ${article.join('\n    ')}`;
+//     writeFileSync(outputFile, text); // 将text写入outputFile文件中
   
-    return outputFile;
-  }
+//     return outputFile;
+//   }
 
-saveCorpus(title,article);
+// saveCorpus(title,article);
 
 // const url = import.meta.url; // 获取当前脚本文件的url
 // // url 是 Node.js 的内置模块，用来解析 url 地址。fileURLToPath 是这个模块的方法，可以将 url 转为文件路径。然后再通过内置模块 path 的 dirname 方法就可以取到当前 JS 文件目录
